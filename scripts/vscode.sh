a4441d6b2c85f5d1712a38c26d50d597fb92690f
EvanGu <gdp12315@163.com>
2018-01-17 21:51:29 +0800

    fix

--- scripts/vscode.sh
+++ scripts/vscode.sh
@@ -12,10 +12,10 @@ fi
 echo ">>> Installing  VSCode";
 
 # Install VSCode
-# curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
-# sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
-# sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
-# sudo apt-get update
-# sudo apt-get -y code
-sudo dpkg -i code_1.19.2-1515599945_amd64.deb
-sudo apt-get install -f 
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+sudo apt-get update
+sudo apt-get -y code
+# sudo dpkg -i code_1.19.2-1515599945_amd64.deb
+# sudo apt-get install -f 
