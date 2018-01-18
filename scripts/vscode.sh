fe2383535841924fe95702bb96b7e4758f82f009
EvanGu <gdp12315@163.com>
2018-01-18 08:37:22 +0800

    fix

--- scripts/vscode.sh
+++ scripts/vscode.sh
@@ -16,6 +16,6 @@ curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microso
 sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
 sudo apt-get update
-sudo apt-get -y code
+sudo apt-get install -y code
 # sudo dpkg -i code_1.19.2-1515599945_amd64.deb
 # sudo apt-get install -f 
